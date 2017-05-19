90a512965421e881cabc31cafa3cfa9ecb321b32
xtons <greatfox@gmail.com>
2017-05-19 16:32:29 +0800

    分组保存规则

--- gfw2re.py
+++ gfw2re.py
@@ -21,11 +21,25 @@ def gfw2re(filename=''):
     }
   }
   if filename=='':
-    content = b64decode(request.urlopen(_gwlurl).read())
+    content = b64decode(request.urlopen(_gwlurl).read()).decode('utf-8')
   else:
-    content = b64decode(open(filename).read())
+    content = b64decode(open(filename).read()).decode('utf-8')
   for line in content.splitlines():
-    print( line )
+    if line.startswith('@@'):
+      line = line[2:]
+      list = sites['white']
+    else:
+      list = sites['black']
+    if line.startswith('!'):
+      continue
+    elif line.startswith('||'):
+      list['type2'].append(line[2:])
+    elif line.startswith('|'):
+      list['type1'].append(line[1:])
+    else:
+      list['type3'].append(line)
+  # 句法有些不明确之处
+  print( sites['black']['type3'] )
   return filename;
 
 def getUsage():
