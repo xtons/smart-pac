bc41b712fdfbfa0bdd88655c880df69b9fe9685b
Liang Dong <greatfox@gmail.com>
2018-04-24 00:13:00 +0800

    convert to node.js version

--- smart-pac.js
+++ smart-pac.js
@@ -1,3 +1,62 @@
 #!/usr/bin/env node
 
-console.log('This is a test.');
+var fs = require("fs");
+const ProgressBar = require('progress');
+const https = require('https');
+const stream = require('stream');
+const readline = require('readline');
+const ejs = require('ejs');
+
+
+const apnicurl = "https://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest";
+var chsips = [];
+https.get(apnicurl).on('response', (res) => {
+  var len = parseInt(res.headers['content-length'], 10);
+  var bar = new ProgressBar('  Downloading apnic records [:bar] :rate/bps :percent :etas', {
+    complete: '=',
+    incomplete: '-',
+    width: 50,
+    stream: process.stderr,
+    total: len
+  });
+  const reVer = /^\d.*$/;
+  const reCN = /^apnic\|CN\|ipv4\|((\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3}))\|(\d+)\|\d{8}\|.*$/
+  const rl = readline.createInterface({
+    input: res,
+    output: null
+  }).on('line', (line) => {
+    // 跳过注释、空行与开头的版本说明
+    if (line.startsWith('#') || line.length == 0)
+      return;
+    if (reVer.exec(line))
+      if (!line.startsWith('2|'))
+        throw "found a unsupported version.";
+    // 区分黑名单与白名单
+    const result = reCN.exec(line)
+    if (result) {
+      const sect = [parseInt(result[2], 10) << 24 | parseInt(result[3], 10) << 16 | parseInt(result[4], 10) << 8 | parseInt(result[5], 10),
+        parseInt(result[6], 10)];
+      if (chsips.length == 0) {
+        chsips.push(sect);
+      }
+      else {
+        const last = chsips[chsips.length - 1];
+        if (last[0] + last[1] == sect[0])
+          last[1] += sect[1];
+        else
+          chsips.push(sect);
+      }
+    }
+  });
+  res.on('data', (chunk) => {
+    bar.tick(chunk.length);
+  });
+  res.on('end', () => {
+    ejs.renderFile(__dirname+'/smart.template',{"chsips":JSON.stringify(chsips)},(err,data)=>{
+      console.log( err||data);
+    });
+  });
+}).on('error', (err) => {
+  console.log('\nGet apnic records failed!\n');
+  console.log(err);
+});
