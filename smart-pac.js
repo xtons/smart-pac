e3ba910ba1b5944cc08697b07290c9e6ec645819
Liang Dong <greatfox@gmail.com>
2018-04-24 02:01:39 +0800

    获取GFW List并对数据流进行base64解码

--- smart-pac.js
+++ smart-pac.js
@@ -1,15 +1,45 @@
 #!/usr/bin/env node
 
-var fs = require("fs");
+const fs = require("fs");
 const ProgressBar = require('progress');
 const https = require('https');
-const stream = require('stream');
 const readline = require('readline');
 const ejs = require('ejs');
+const base64 = require('base64-stream');
 
 
 const apnicurl = "https://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest";
-var chsips = [];
+const gfwlisturl = "https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt";
+
+var smart = {
+  chsips: []
+};
+
+https.get(gfwlisturl).on('response', (res) => {
+  if (res.statusCode === 200) {
+    const len = parseInt(res.headers['content-length'], 10);
+    const bar = new ProgressBar('  Downloading GFW List      [:bar] :rate/bps :percent :etas', {
+      complete: '=',
+      incomplete: '-',
+      width: 50,
+      stream: process.stderr,
+      total: len
+    });
+    readline.createInterface({
+      input: res.pipe(base64.decode()),
+      output: null
+    }).on('line', (line) => {
+      //console.log(line);
+    });
+    res.on('data', (chunk) => {
+      bar.tick(chunk.length);
+    })
+  }
+}).on('error', (err) => {
+  console.log('\nGet GFW List failed!\n');
+  console.log(err);
+});
+
 https.get(apnicurl).on('response', (res) => {
   var len = parseInt(res.headers['content-length'], 10);
   var bar = new ProgressBar('  Downloading apnic records [:bar] :rate/bps :percent :etas', {
@@ -34,17 +64,19 @@ https.get(apnicurl).on('response', (res) => {
     // 区分黑名单与白名单
     const result = reCN.exec(line)
     if (result) {
-      const sect = [parseInt(result[2], 10) << 24 | parseInt(result[3], 10) << 16 | parseInt(result[4], 10) << 8 | parseInt(result[5], 10),
+      with (smart) {
+        const sect = [parseInt(result[2], 10) << 24 | parseInt(result[3], 10) << 16 | parseInt(result[4], 10) << 8 | parseInt(result[5], 10),
         parseInt(result[6], 10)];
-      if (chsips.length == 0) {
-        chsips.push(sect);
-      }
-      else {
-        const last = chsips[chsips.length - 1];
-        if (last[0] + last[1] == sect[0])
-          last[1] += sect[1];
-        else
+        if (chsips.length == 0) {
           chsips.push(sect);
+        }
+        else {
+          const last = chsips[chsips.length - 1];
+          if (last[0] + last[1] == sect[0])
+            last[1] += sect[1];
+          else
+            chsips.push(sect);
+        }
       }
     }
   });
@@ -52,8 +84,8 @@ https.get(apnicurl).on('response', (res) => {
     bar.tick(chunk.length);
   });
   res.on('end', () => {
-    ejs.renderFile(__dirname+'/smart.template',{"chsips":JSON.stringify(chsips)},(err,data)=>{
-      console.log( err||data);
+    ejs.renderFile(__dirname + '/smart.template', smart, (err, data) => {
+      console.log(err || data);
     });
   });
 }).on('error', (err) => {
